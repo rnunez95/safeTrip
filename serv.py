d125e4d4bfaca20ce6d3a091d3505298468aab84
Rafael <rafael95nunez@gmail>
2020-10-29 20:58:39 -0400

    init

---
+++ serv.py
@@ -0,0 +1,18 @@
+from flask import Flask, escape, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+@app.route('/data')
+def data():
+    data={
+        "id": 1,
+        "user": "John Doe",
+        "location": "Somewhere"
+    }
+    return json.dumps(data)
